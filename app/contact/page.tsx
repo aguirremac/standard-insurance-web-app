e784006a890bdccab15b2ba7e7418520a60f5b33
aguirremac <markjohnaguirre@gmail.com>
2026-02-27 13:59:24 +0800

    chore: test

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,6 +3,8 @@
 import { Header } from "@/components/Header";
 import { ContactSection } from "@/components/ContactSection";
 import { Footer } from "@/components/Footer";
+import { Metadata } from "next";
+
 
 export default function App() {
   return (
